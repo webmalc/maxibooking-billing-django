0578a8d8924239e5a4d4837c3413a624a294a254
webmalc <webmalc@gmail.com>
2017-07-18 14:15:03 +0300

    clients model

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,39 @@
-# from django.db import models
-# from django_extensions.db.models import TimeStampedModel
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
+from phonenumber_field.modelfields import PhoneNumberField
 
-# class Hotel(TimeStampedModel):
-#     pass
+from billing.models import CommonInfo
+
+
+class Client(CommonInfo, TimeStampedModel):
+    """
+    Client class
+    """
+    STATUSES = (('not_confirmed', _('not confirmed')), ('active', _('active')),
+                ('disabled', _('disabled')), ('archived', _('archived')))
+
+    login = models.CharField(
+        max_length=50,
+        unique=True,
+        db_index=True,
+        validators=[MinLengthValidator(6)])
+    email = models.EmailField(
+        db_index=True, unique=True, verbose_name=_('e-mail'))
+    phone = PhoneNumberField(max_length=50, db_index=True)
+    name = models.CharField(
+        max_length=255, db_index=True, validators=[MinLengthValidator(2)])
+    description = models.TextField(
+        null=True,
+        blank=True,
+        db_index=True,
+        validators=[MinLengthValidator(2)])
+    status = models.CharField(
+        max_length=20,
+        default='not_confirmed',
+        choices=STATUSES,
+        db_index=True)
+
+    class Meta:
+        ordering = ['-created']
