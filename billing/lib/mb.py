647406f1993670dc1a5325e21718e04c38a35321
webmalc <webmalc@gmail.com>
2017-07-25 18:22:24 +0300

    mb service: client install

--- billing/lib/mb.py
+++ billing/lib/mb.py
@@ -1,4 +1,9 @@
 import logging
+import time
+
+import requests
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
 
 def install_client(client):
@@ -8,3 +13,28 @@ def install_client(client):
     logging.getLogger('billing').info(
         'Begin client installation. Id: {}; login: {}'.format(client.id,
                                                               client.login))
+    i = 0
+    while i < 10:
+        i += 1
+        try:
+            response = requests.post(
+                settings.MB_URL,
+                timeout=20,
+                json={
+                    'client':
+                    client.login,
+                    'results_url':
+                    reverse('client-install-result', args=[client.login])
+                })
+            if response.status_code == 200:
+                break
+        except requests.exceptions.RequestException:
+            pass
+
+        if settings.DEBUG:
+            time.sleep(settings.MB_TIMEOUT)
+
+    else:
+        logging.getLogger('billing').info(
+            'Failed client installation. Id: {}; login: {}'.format(
+                client.id, client.login))
