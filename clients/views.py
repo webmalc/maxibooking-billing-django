d9f5c3e246ce3146bf83fe831ac5a3cfe346024d
webmalc <m@webmalc.pw>
2018-01-23 14:20:52 +0300

    fixtures view fix

--- clients/views.py
+++ clients/views.py
@@ -188,7 +188,7 @@ class ClientViewSet(viewsets.ModelViewSet):
             })
 
         response = mb.client_fixtures(client)
-        if response and response['status']:
+        if response and response.get('status', True):
             return Response({
                 'status': True,
                 'message': 'client fixtures installed',
