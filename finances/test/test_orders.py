f96363a0d998115e987b91f9a39f7dae34df9742
webmalc <m@webmalc.pw>
2019-02-05 13:41:46 +0300

    remove the client name from all the emails

--- finances/test/test_orders.py
+++ finances/test/test_orders.py
@@ -108,7 +108,6 @@ def test_order_creation_and_modifications(mailoutbox):
     assert mail.recipients() == ['user@one.com']
     # assert '20 more days' in html
     assert '3 more days' in html
-    assert 'User One' in html
 
     order.client_services.add(1, 2)
     assert order.price == Money(14001.83, EUR)
@@ -181,7 +180,6 @@ def test_orders_payment_notification(make_orders, mailoutbox):
     assert mail.recipients() == ['user@one.com']
     assert 'Order will expire soon' in mail.subject
     assert 'We glad to see' not in html
-    assert 'User One' in html
 
     Order.objects.filter(client_id=1, status='paid').delete()
     orders_payment_notify.delay()
@@ -247,7 +245,6 @@ def test_orders_clients_disable(make_orders, mailoutbox):
     mailoutbox = [m for m in mailoutbox if 'account is disabled' in m.subject]
     assert len(mailoutbox) == 1
     assert mailoutbox[0].recipients() == ['user@one.com']
-    assert 'User One' in mailoutbox[-1].alternatives[0][0]
     assert '#3' in mailoutbox[-1].alternatives[0][0]
 
     client = Client.objects.get(login='user-one')
@@ -295,7 +292,6 @@ def test_order_paid_email(make_orders, mailoutbox):
 
     assert mailoutbox[-1].recipients() == ['user@one.com']
     assert 'Your payment was successful' in mailoutbox[-1].subject
-    assert 'User One' in mailoutbox[-1].alternatives[0][0]
     assert '#1' in mailoutbox[-1].alternatives[0][0]
 
     order_ru.status = 'paid'
@@ -303,5 +299,4 @@ def test_order_paid_email(make_orders, mailoutbox):
 
     assert mailoutbox[-1].recipients() == ['user@rus.com']
     assert 'Успешная оплата' in mailoutbox[-1].subject
-    assert 'user rus' in mailoutbox[-1].alternatives[0][0]
     assert '№5' in mailoutbox[-1].alternatives[0][0]
